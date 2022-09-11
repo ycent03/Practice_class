34cb3d5e7c688f76fcedf4f0b23c500324422fa5
Linux <adebisio024@gmail.com>
2022-09-11 02:43:07 +0100

    input

--- time_for_c/number_line.c
+++ time_for_c/number_line.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+i#include <stdio.h>
 #include <stdlib.h>
 /**
  * main - Entry point
@@ -6,10 +6,10 @@
  * Reurn: Always 0 (Success)
  */
 int main(void)
-	char Name[100];
 {
+	char Name[100];
 		printf("Enter your name: ");
 		fgets(Name, 100, stdin);
-		printf("Hello user your name is %s\n", Name)
+		printf("Hello user your name is %s\n", Name);
 	return (0);
 }
