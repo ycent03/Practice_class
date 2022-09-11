a36ef1cfcc2b045fb569491e0581e324051e58c7
Linux <adebisio024@gmail.com>
2022-09-11 01:11:06 +0100

    pratice

---
+++ pratice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descripition: 'printing'
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+ {
+ 	printf("We don't have a name for our group yet\n");
+	
+	return (0);
+ }
